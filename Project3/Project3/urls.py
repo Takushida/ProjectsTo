ee1b3293051cf55b02939745eb394b395ab210ff
Takushida <gigandreig@gmail.com>
2023-09-14 06:43:01 +0100

    14.09

--- Project3/Project3/urls.py
+++ Project3/Project3/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
-    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
-    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete')
+    path('', include('posts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
