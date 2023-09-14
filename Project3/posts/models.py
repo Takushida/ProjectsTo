baa44de46047ff6bab1bb2500795c6e125ab62ef
Takushida <gigandreig@gmail.com>
2023-09-14 07:08:38 +0100

    14.09

--- Project3/posts/models.py
+++ Project3/posts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.urls import reverse
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True)
-    body = models.TextField()
+    title = models.CharField(max_length=200, verbose_name=u'Название')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, verbose_name=u'Автор')
+    body = models.TextField(verbose_name=u'Текст')
 
     def __str__(self):
         return self.title
